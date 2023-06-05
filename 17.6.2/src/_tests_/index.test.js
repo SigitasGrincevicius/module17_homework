5ceceee5d9157a8cdb32c2e4d6c32c0741abe38d
Sigitas Grincevičius <s.grincevicius@gmail.com>
2023-06-05 17:31:07 +0300

    test file fix

--- 17.6.2/src/_tests_/index.test.js
+++ 17.6.2/src/_tests_/index.test.js
@@ -5,12 +5,12 @@ describe('Phone format validation function tests', () => {
       const phoneNumber = '+7 (987) 654-32-10';
       const result = validatePhoneNumbers(phoneNumber);
       expect(result).toBe(true);
-   });
+   }),
    it('validation of phone number +7 (4212) 53-53-53 to be true', () => {
       const phoneNumber = '+7 (4212) 53-53-53';
       const result = validatePhoneNumbers(phoneNumber);
       expect(result).toBe(true);
-   });
+   }),
    it('validation of phone number +370 (652) 98-100 to be false', () => {
       const phoneNumber = '+370 (652) 98-100';
       const result = validatePhoneNumbers(phoneNumber);
